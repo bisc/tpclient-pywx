4e9f3d462f2e233c9ee440966983519209f9da58
Ivan bisc Ruchkin <ruchkin.ivan@gmail.com>
2010-06-28 01:21:00 +0400

    Refactoring, comments, normal html generation.

--- windows/main/winMessageBrowser.py
+++ windows/main/winMessageBrowser.py
@@ -20,25 +20,41 @@ class winMessageBrowser(winReportXRC, winMessageBrowserBase):
 
 		self.application = application
 
+		# FIXME integrate it with panel Messages data
+		self.html_message = """\
+<html>
+<body>
+<center>
+	<table cols=1 width="100%%">
+		<tr>
+			<td><b>Subject:</b> %(subject)s</td>
+		</tr>
+		<tr>
+			<td>%(body)s</td>
+		</tr>
+	</table>
+</center>
+</body>
+</html>"""
+
 		self.application.gui.Binder(self.application.CacheClass.CacheUpdateEvent, self.OnCacheUpdate)
-		self.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnItemActivated, self.Boards)
+		self.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnItemClicked, self.Boards)
 
 	def OnCacheUpdate(self, evt):
-		print("____________________________")
-		print("1. %s" % self.application.cache.boards)
-		print("2. %s" % self.application.cache.boards[0])
-		print("3. %s" % self.application.cache.messages)
-		print("4. %s" % self.application.cache.messages[0])
-
+		# Clear all
 		self.Boards.DeleteAllItems()
 		self.item_to_message = {}
 		self.item_to_board = {}
-		
+
+		# Create the tree of boards and messages
 		root = self.Boards.AddRoot('root')
 		for bid in self.application.cache.boards.keys():
+			# Create each board
 			messages = self.application.cache.messages[bid]
 			board_item = self.Boards.AppendItem(root, self.application.cache.boards[bid].name)
 			self.item_to_board[board_item] = self.application.cache.boards[bid]
+
+			# Append messages for each board
 			for i, msg in enumerate(messages):
 				message_item = self.Boards.AppendItem(board_item, msg.CurrentOrder.subject)
 				self.item_to_message[message_item] = msg.CurrentOrder
@@ -46,21 +62,26 @@ class winMessageBrowser(winReportXRC, winMessageBrowserBase):
 
 		self.Boards.Expand(root)
 
-	def OnItemActivated(self, evt):
-		print("board: %s\n messages: %s" % (self.item_to_board, self.item_to_message))
+	def OnItemClicked(self, evt):
+		"""
+		Called when an item in the board tree is clicked (equal to selection change event)
+		if we have the single item selection style.
+		"""
 		selected_item = self.Boards.GetSelection()
 		print("selected: %s " % selected_item)
 
 		# Need to compare tree item ids directly, not through has_key
-		# Find among boards
+		# Find among boards, do nothing
 		for key in self.item_to_board.keys():
 			if key == selected_item:
 				print("board selected, dunno what to do")
 
-		# Find among messages
+		# Find among messages, set text
 		for key in self.item_to_message.keys():
 			if key == selected_item:
-				self.Message.SetPage(self.item_to_message[key].body)
+				msg = self.item_to_message[key]
+				self.Message.SetPage(self.html_message % {'subject' : msg.subject, 'body' : msg.body})
+				break
 
 
 
