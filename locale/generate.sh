2c5535edff3d8fb1446e1d9bdda37689b945f34c
Ivan bisc Ruchkin <ruchkin.ivan@gmail.com>
2010-09-16 18:07:18 +0400

    Modified translation strings generation script.
    Now it doesn't search for strings in ../build dir.

--- locale/generate.sh
+++ locale/generate.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 cd ..
-xgettext -j -L python --from-code utf-8 -o ./locale/tpclient-pywx.pot `find -name \*.py` tpclient-pywx ./doc/tips.txt
+xgettext -j -L python --from-code utf-8 -o ./locale/tpclient-pywx.pot `find -name \*.py ! -path './build/*'` tpclient-pywx ./doc/tips.txt
 
